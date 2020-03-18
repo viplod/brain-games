543ad4a4e3181e3aad2f3b9b3b329db006b0e698
Viplod <viplod@yandex.ru>
2020-03-18 13:07:18 +0500

    checked linter

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,16 +3,12 @@ from brain_games.cli import welcome_user
 from brain_games.even import even
 
 
-
-
 def brain_even():
-	main()
-	print('Answer "yes" if number even otherwise answer "no".')
-	name = welcome_user()
-	result = even()
-	if result:
-		print('Congratulations,', name)
-	else:
-		print("Let's try again,", name ,"!")
-
-
+    main()
+    print('Answer "yes" if number even otherwise answer "no".')
+    name = welcome_user()
+    result = even()
+    if result:
+        print('Congratulations,', name)
+    else:
+        print("Let's try again,", name, "!")
