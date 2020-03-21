b598c22f1e9b1fbbd6e1a16f1366d4e1d031796d
Viplod <viplod@yandex.ru>
2020-03-22 01:17:48 +0500

    error correction

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,15 +3,16 @@ import prompt
 
 
 def divisor(val1, val2):
-    for index in range(1, val1):
+    maxdivisor = 1
+    for index in range(1, val1 +1):
         if (val1 % index == 0) and (val2 % index == 0):
             maxdivisor = index
     return maxdivisor
 
 def gcd():
     for _ in range(0, 3):
-        number1 = random.randint(0, 99)
-        number2 = random.randint(0, 99)
+        number1 = random.randint(1, 99)
+        number2 = random.randint(1, 99)
         print('Question ', str(number1), str(number2))
         answer = prompt.integer('You answer: ')
         if number1 > number2:
